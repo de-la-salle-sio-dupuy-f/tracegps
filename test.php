9ec6205692762750600e47744babc6fcceceb96e
delasalle-sio-gasnier-v <delasalle.sio.gasnier.v@gmail.com>
2020-10-06 16:36:58 +0200

    test1

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-<<<<<<< HEAD
-// coucou c'est rien c'est les soucis
-
-=======
-// création du fichier potos
->>>>>>> branch 'master' of https://github.com/de-la-salle-sio-dupuy-f/tracegps.git
-?>
